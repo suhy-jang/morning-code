a7bc7ae8b426d4f6123e20fa7deeaedb5b74c7ee
shjang7 <lucy.sh.jang@gmail.com>
2019-06-28 03:01:40 +0900

    Create suhy.rb

---
+++ ruby/7.5 Counting Sort 1/suhy.rb
@@ -0,0 +1,24 @@
+def count_add(arr)
+  arr.each_with_object(Hash.new(0)) { |elem, counts| counts[elem] += 1 }
+end
+
+def stack_counts(counts)
+  (0..counts.max[0]).each_with_index do |key,idx|
+    counts[idx] += counts[idx-1] unless idx.zero?
+  end
+  counts
+end
+
+def counting_sort(array)
+  counts = Array.new
+  (0..array.max).each do |idx|
+    counts << 0
+  end
+  array.each do |elem|
+    counts[elem] += 1
+  end
+  counts
+end
+
+p counting_sort([63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33])
+# => [0, 2, 0, 2, 0, 0, 1, 0, 1, 2, 1, 0, 1, 1, 0, 0, 2, 0, 1, 0, 1, 2, 1, 1, 1, 3, 0, 2, 0, 0, 2, 0, 3, 3, 1, 0, 0, 0, 0, 2, 2, 1, 1, 1, 2, 0, 2, 0, 1, 0, 1, 0, 0, 1, 0, 0, 2, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 2, 1, 3, 2, 0, 0, 2, 1, 2, 1, 0, 2, 2, 1, 2, 1, 2, 1, 1, 2, 2, 0, 3, 2, 1, 1, 0, 1, 1, 1, 0, 2, 2]
---
+++ ruby/7.7 Prepare for Full Counting Sort/suhy.rb
@@ -0,0 +1,14 @@
+def full_counting_sort_prep(array)
+  counts = get_counts(array)
+  counts.each_with_index do |element, index|
+    counts[index] += counts[index-1] unless index.zero?
+  end
+  counts
+end
+
+def get_counts(array)
+  array.each_with_object(Array.new(100,0)) { |element,counts| counts[element.to_i] += 1 }
+end
+
+p full_counting_sort_prep(["4 that", "3 be", "0 to", "1 be", "5 question", "1 or", "2 not", "4 is", "2 to", "4 the"])
+# => [1, 3, 5, 6, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
