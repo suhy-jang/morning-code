e2d56b6a8c89f3fd5ac3b0a451a454259f596fb2
Suhyeon <lucy.sh.jang@gmail.com>
2019-07-02 18:22:58 +0900

    02_jul_suhy Upload new code

--- ruby/7.11 Quick Sort Advanced/suhy.rb
+++ ruby/7.11 Quick Sort Advanced/suhy.rb
@@ -1,1 +1,24 @@
-# example
+def advanced_quicksort(array)
+  advanced_quicksort_recursive(array, 0, array.length - 1, array.length - 1)
+end
+
+def advanced_quicksort_recursive(array, i_start, i_end, i_pivot )
+  return array if i_start >= i_end
+  i_lg = i_start
+  i_start.upto(i_end) do |index|
+    if array[index] <= array[i_pivot]
+      array[index], array[i_lg] = array[i_lg], array[index]
+      i_lg += 1
+    end
+  end
+  i_lg -= 1
+  puts array.join(" ")
+  advanced_quicksort_recursive(array, i_start, i_lg - 1, i_lg - 1)
+  advanced_quicksort_recursive(array, i_lg+1, i_end, i_end)
+end
+
+# advanced_quicksort([9, 8, 6, 7, 3, 5, 4, 1, 2])
+advanced_quicksort([1, 3, 9, 8, 2, 7, 5])
+# => 1 3 2 5 9 7 8
+#    1 2 3 5 9 7 8
+#    1 2 3 5 7 8 9
