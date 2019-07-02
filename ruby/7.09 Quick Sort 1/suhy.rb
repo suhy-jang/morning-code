39753b925e1102569bfe42f92c6370593298d168
Suhyeon <lucy.sh.jang@gmail.com>
2019-07-02 10:57:34 +0900

    02_jul_suhy Upload new

--- ruby/7.09 Quick Sort 1/suhy.rb
+++ ruby/7.09 Quick Sort 1/suhy.rb
@@ -1,1 +1,15 @@
-# example
+def partition(array)
+  pivot = array[0]
+  lefts = get_combine(array, pivot, -1)
+  rights = get_combine(array, pivot, 1)
+  lefts.concat([pivot], rights)
+end
+
+def get_combine(array, pivot, sign)
+  array.each_with_object(Array.new) do |element, new_arr| 
+    new_arr << element if sign * (element - pivot) > 0
+  end
+end
+
+p partition([4, 5, 3, 9, 1])
+# => [3, 1, 4, 5, 9]
