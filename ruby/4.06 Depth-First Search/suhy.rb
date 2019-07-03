ddf75994acdf48bca55676a9e84fc772a3ab88bd
shjang7 <lucy.sh.jang@gmail.com>
2019-07-03 21:48:44 +0900

    Update suhy.rb to improve performance
    for checking visited O(1) instead of O(n)

--- ruby/4.06 Depth-First Search/suhy.rb
+++ ruby/4.06 Depth-First Search/suhy.rb
@@ -1,23 +1,30 @@
+def DFS_helper(graph, idx, visited)
+  return [] if visited[idx]
 
-def DFS_helper(graph, idx, result)
-  result << idx unless result.include?(idx)
+  results = [idx]
+  visited[idx] = true
   for next_idx in graph[idx]
-    DFS_helper(graph,next_idx,result) unless result.include?(next_idx)
+    results.concat(DFS_helper(graph,next_idx,visited))
   end
-  return result
+  return results
 end
 
 def depth_first_search(graph)
   return nil if graph.nil?
-  DFS_helper(graph,0,[])
+  
+  DFS_helper(graph, 0, create_false_array(graph))
+end
+
+def create_false_array(array)
+  Array.new(array.length, false)
 end
 
 p depth_first_search({
-  0 => [2], 
-  1 => [4], 
-  2 => [5, 0, 3], 
-  3 => [2], 
-  4 => [1, 5], 
+  0 => [2],
+  1 => [4],
+  2 => [5, 0, 3],
+  3 => [2],
+  4 => [1, 5],
   5 => [4, 2]
 })
 # => [0, 2, 5, 4, 1, 3]
