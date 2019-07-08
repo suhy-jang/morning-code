5ff9370148f59fc449f4703d98ad9ceda0006564
grace-mugoiri <gracemugoiri@gmail.com>
2019-07-08 11:29:13 +0300

    added index match with binary search

--- ruby/8.03 Index Match/grace.rb
+++ ruby/8.03 Index Match/grace.rb
@@ -6,3 +6,18 @@ end
 
 puts index_match([0, 2, 3, 7, 9, 11])
 # => 0
+
+def binary_search(array, low, high)
+    return -1 if low > high
+    middle = (low + high) / 2
+    el = array[middle]
+    return el if el == middle
+    return binary_search(array, low, middle-1) if el > middle
+    return binary_search(array, middle+1, high) if el < middle
+end
+def index_match(array)
+    low = 0
+    high = array.size - 1
+    return binary_search(array, low, high)
+end
+puts index_match([0, 2, 3, 7, 9, 11])
