5c6dd2480ab61567de84cf5a1284e8dd730c920c
Suhyeon <lucy.sh.jang@gmail.com>
2019-07-11 18:07:17 +0900

    Update suhy.rb

--- ruby/8.06 Smallest Difference/suhy.rb
+++ ruby/8.06 Smallest Difference/suhy.rb
@@ -1,16 +1,16 @@
 def smallest_difference(array)
   array.sort!
   min_val = nil
-  range = nil
+  min_pair = nil
   
   array.each_index do |index|
     curr_val = (array[index] - array[index - 1]).abs
     if min_val.nil? || min_val > curr_val
       min_val = curr_val
-      range = [array[index - 1], array[index]]
+      min_pair = [array[index - 1], array[index]]
     end
   end
-  range
+  min_pair
 end
 
 p smallest_difference([-20, -3916237, -357920, -3620601, 7374819, -7330761, 30, 6246457, -6461594, 266854])
