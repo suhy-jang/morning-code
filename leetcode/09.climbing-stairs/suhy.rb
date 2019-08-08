b6abbb5a82cda2c6b37643954b07d3fce443d2bf
Suhyeon <lucy.sh.jang@gmail.com>
2019-08-08 17:24:10 +0900

    Update suhy.rb

--- leetcode/09.climbing-stairs/suhy.rb
+++ leetcode/09.climbing-stairs/suhy.rb
@@ -2,21 +2,22 @@
 
 # @param {Integer} n
 # @return {Integer}
-$memo = []
+# @param {Integer} n
+# @return {Integer}
 
 def climb_stairs(n)
-    $memo = Array.new(n)
-    climb_stairs_h(n)
+    memo = Array.new(n)
+    climb_stairs_h(n, memo)
 end
 
-def climb_stairs_h(n)
-    return $memo[n] if $memo[n]
+def climb_stairs_h(n, memo)
+    return memo[n] if memo[n]
     if n<=2
-        $memo[n] = n
+        memo[n] = n
     else
-        $memo[n] = climb_stairs_h(n-2) + climb_stairs_h(n-1)
+        memo[n] = climb_stairs_h(n-2, memo) + climb_stairs_h(n-1, memo)
     end
-    $memo[n]
+    memo[n]
 end
 
 # 0->0, 1->1, 2->2, 3->3
