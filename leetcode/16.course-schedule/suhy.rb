75258d14d72ce714eb061c534d1077dd19104160
Suhyeon <lucy.sh.jang@gmail.com>
2019-08-30 18:06:11 +0900

    Update suhy.rb
    improve calculation time

--- leetcode/16.course-schedule/suhy.rb
+++ leetcode/16.course-schedule/suhy.rb
@@ -1,33 +1,38 @@
+# https://leetcode.com/problems/course-schedule/
 # @param {Integer} num_courses
 # @param {Integer[][]} prerequisites
 # @return {Boolean}
 def can_finish(num_courses, prerequisites)
-    graph = Array.new{ Array.new }
+    graph = Array.new { Array.new }
     visited = Array.new(num_courses)
     prerequisites.each do |pair|
-        while graph.size <= pair[1]
-            inner = Array.new
-            graph << inner
+        if graph.size <= pair[1]
+            add = Array.new(pair[1] - graph.size + 1) { Array.new }
+            graph += add
         end
         graph[pair[1]] << pair[0]
     end
     (0...num_courses).each do |i|
         return false unless dfs(graph, visited, i)
     end
-    return true
-end
+    true
+end        
 
 def dfs(graph, visited, i)
     return false if visited[i] == -1
     return true if visited[i] == 1
     if graph.size <= i
-        visited[i] = 1
+        visited[i] = true
         return true
     end
     visited[i] = -1
-    graph[i].each do |index|
-        return false unless dfs(graph, visited, index)
+    graph[i].each do |connect|
+        return false unless dfs(graph, visited, connect)
     end
     visited[i] = 1
     true
 end
+
+# input : 2
+#         [[1,0],[0,1]]
+# output : false
