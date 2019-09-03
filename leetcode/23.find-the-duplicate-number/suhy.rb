1ed8340e5f593b33121b33961376bda2f7486923
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-09-03 18:02:32 +0900

    Update suhy.rb
    tortoise and hare solution

--- leetcode/23.find-the-duplicate-number/suhy.rb
+++ leetcode/23.find-the-duplicate-number/suhy.rb
@@ -1,9 +1,18 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def find_duplicate(nums)
-    nums.sort!
-    nums.each_index do |i|
-        return nums[i] if nums[i-1] == nums[i] || nums[i] == nums[i+1]
+    hare = nums[0]
+    tortoise = nums[0]
+    loop do
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        break if tortoise == hare
     end
-    nil
+    ptr1 = nums[0]
+    ptr2 = tortoise
+    until ptr1 == ptr2
+        ptr1 = nums[ptr1]
+        ptr2 = nums[ptr2]
+    end
+    return ptr1
 end
