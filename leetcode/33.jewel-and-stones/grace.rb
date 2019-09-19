4859146b6a03930560809b176d2a0ec356ccda2c
grace-mugoiri <gracemugoiri@gmail.com>
2019-09-19 11:26:47 +0300

    jewels and stones solution

---
+++ leetcode/33.jewel-and-stones/grace.rb
@@ -0,0 +1,11 @@
+# @param {String} j
+# @param {String} s
+# @return {Integer}
+def num_jewels_in_stones(j, s)
+    set = Set.new(j.chars)
+    count = 0
+    s.each_char do |stone|
+     count += 1 if set.include?(stone)
+    end
+    return count 
+ end
