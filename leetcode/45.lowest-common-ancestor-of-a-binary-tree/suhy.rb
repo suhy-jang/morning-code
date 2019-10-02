022b07bbeca9ee4e64259ce54c97126a8cbff7ed
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-10-02 17:56:39 +0900

    Create suhy.rb

---
+++ leetcode/45.lowest-common-ancestor-of-a-binary-tree/suhy.rb
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+# @param {TreeNode} root
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {TreeNode}
+def lowest_common_ancestor(root, p, q)
+    return nil if root.nil?
+    lca = []
+    (root == p || root == q) ? lca << root : lca << nil
+    lca << lowest_common_ancestor(root.left, p, q)
+    lca << lowest_common_ancestor(root.right, p, q)
+    return root if (!!lca[0] && (lca[1] || lca[2])) || (!!lca[1] && !!lca[2])
+    return root if (!!lca[0] && !lca[1] && !lca[2])
+    return lca[1] || lca[2]
+end
