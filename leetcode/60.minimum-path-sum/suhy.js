edbd396745386e6340c64f7d4ac2ec1e17c511a7
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-10-22 17:33:00 +0900

    Create suhy.js

---
+++ leetcode/60.minimum-path-sum/suhy.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+    const rLength = grid.length;
+    const cLength = grid[0].length;
+    const minGrid = new Array(rLength).fill(new Array(cLength).fill(0));
+    for(let r=0; r<rLength; r++) {
+        for(let c=0; c<cLength; c++) {
+            if (r-1 >= 0) minGrid[r][c] = minGrid[r-1][c];
+            if (c-1 >= 0) minGrid[r][c] = (minGrid[r][c] === 0 || minGrid[r][c] > minGrid[r][c-1]) ? minGrid[r][c-1] : minGrid[r][c];
+            minGrid[r][c] += grid[r][c];
+        }
+    }
+    return minGrid[rLength-1][cLength-1];
+};
