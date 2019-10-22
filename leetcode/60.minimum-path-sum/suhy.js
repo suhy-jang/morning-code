d9d81ff30e132d37ea06eeef7fd1f0108ad3e84d
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-10-22 17:48:48 +0900

    Update min-path-sum memory less usage

--- leetcode/60.minimum-path-sum/suhy.js
+++ leetcode/60.minimum-path-sum/suhy.js
@@ -5,17 +5,17 @@
 var minPathSum = function(grid) {
     const rLength = grid.length;
     const cLength = grid[0].length;
-    const minGrid = new Array(rLength).fill(new Array(cLength).fill(0));
+    const minGrid = new Array(2).fill(new Array(cLength).fill(0));
     for(let r=0; r<rLength; r++) {
         for(let c=0; c<cLength; c++) {
             if (r-1 >= 0) {
-                minGrid[r][c] = minGrid[r-1][c];
+                minGrid[r%2][c] = minGrid[(r+1)%2][c];
             }
-            if (c-1 >= 0 && (minGrid[r][c] === 0 || minGrid[r][c] > minGrid[r][c-1])) {
-                minGrid[r][c] = minGrid[r][c-1];
+            if (c-1 >= 0 && (minGrid[r%2][c] === 0 || minGrid[r%2][c] > minGrid[r%2][c-1])) {
+                minGrid[r%2][c] = minGrid[r%2][c-1];
             }
-            minGrid[r][c] += grid[r][c];
+            minGrid[r%2][c] += grid[r][c];
         }
     }
-    return minGrid[rLength-1][cLength-1];
+    return minGrid[(rLength+1)%2][cLength-1];
 };
