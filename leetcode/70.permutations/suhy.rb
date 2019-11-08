504537d5e280da8f83a45e12fad10593024486eb
Suhyeon Jang <lucy.sh.jang@gmail.com>
2019-11-08 18:15:23 +0900

    Create suhy.rb

---
+++ leetcode/70.permutations/suhy.rb
@@ -0,0 +1,28 @@
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def swap(nums, i, j)
+    nums[i], nums[j] = nums[j], nums[i]
+end
+
+def backtrack(n, nums, output, first)
+    if first == n
+        output.push Array.new(nums) 
+    end
+    first.upto(n-1) do |i|
+        swap(nums, first, i)
+        backtrack(n, nums, output, first + 1)
+        swap(nums, first, i)
+    end
+end
+
+def permute(nums)
+    output = []
+    
+    nums_list = []
+    nums.each do |num|
+        nums_list.push(num)
+    end
+    n = nums.length
+    backtrack(n, nums_list, output, 0)
+    output
+end
